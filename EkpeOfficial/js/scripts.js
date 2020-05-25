462db31b9f7c658ca791bb1cab9a4e9848f57fc8
stavrosds <63343590+stavrosds@users.noreply.github.com>
2020-05-25 11:45:28 +0300

    Add files via upload

---
+++ EkpeOfficial/js/scripts.js
@@ -0,0 +1,125 @@
+/*!
+    * Start Bootstrap - Freelancer v6.0.0 (https://startbootstrap.com/themes/freelancer)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
+    */
+    (function($) {
+    "use strict"; // Start of use strict
+  
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 71)
+          }, 1000, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+  
+    // Scroll to top button appear
+    $(document).scroll(function() {
+      var scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+  
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+  
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 80
+    });
+  
+    // Collapse Navbar
+    var navbarCollapse = function() {
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-shrink");
+      } else {
+        $("#mainNav").removeClass("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+  
+    // Floating label headings for the contact form
+    $(function() {
+      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+      }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+      }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+      });
+    });
+  
+    // Carousel script
+  $(function() {
+    $('.carousel').carousel({
+      interval: 6000
+    })
+  $(".carousel").on("slide.bs.carousel", function(e) {
+    var prev = $(this)
+      .find(".active")
+      .index();
+    var next = $(e.relatedTarget).index();
+    var video = $("#video-player")[0];
+    var videoSlide = $("#video-player")
+      .closest(".carousel-item")
+      .index();
+    if (next === videoSlide) {
+      if (video.tagName == "IFRAME") {
+        player.playVideo();
+      } else {
+        createVideo(video);
+      }
+    } else {
+      if (typeof player !== "undefined") {
+        player.pauseVideo();
+      }
+    }
+  });
+});
+
+function createVideo(video) {
+  var youtubeScriptId = "youtube-api";
+  var youtubeScript = document.getElementById(youtubeScriptId);
+  var videoId = video.getAttribute("data-video-id");
+
+  if (youtubeScript === null) {
+    var tag = document.createElement("script");
+    var firstScript = document.getElementsByTagName("script")[0];
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    tag.id = youtubeScriptId;
+    firstScript.parentNode.insertBefore(tag, firstScript);
+  }
+
+  window.onYouTubeIframeAPIReady = function() {
+    window.player = new window.YT.Player(video, {
+      videoId: videoId,
+      playerVars: {
+        autoplay: 1,
+        modestbranding: 1,
+        rel: 0
+      }
+    });
+  };
+}
+
+
+
+  })(jQuery); // End of use strict
+  
